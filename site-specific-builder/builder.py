2143e59937be43c648d340792e6ecebf2345593f
Jacob Lin <61257227+CodingFella@users.noreply.github.com>
2025-07-10 17:54:14 -0700

    generate site main files

--- site-specific-builder/builder.py
+++ site-specific-builder/builder.py
@@ -4,8 +4,12 @@ import os
 import shutil
 from datetime import datetime
 
-HEADER_BASE = './header_base.html'
-FOOTER_BASE = './footer_base.html'
+SITE_SPECIFIC_HEADER_BASE = './site_specific/site_specific_header_base.html' 
+SITE_SPECIFIC_FOOTER_BASE = './site_specific/site_specific_footer_base.html'
+
+SITE_MAIN_HEADER_BASE = './site_main/site_main_header_base.html' 
+SITE_MAIN_FOOTER_BASE = './site_main/site_main_footer_base.html'
+
 # Sometimes the file needs some tags at the top to balance out what it contains so the formatter can make it look pretty.
 BEGIN_KEY = '#begin'
 ALL = 'a';
@@ -49,7 +53,7 @@ def render_line(page_layout, data, line, context):
     return modifiedLine;
 
 
-def build_file(page_layout: str):
+def build_file(page_layout: str, header_base: str, footer_base: str):
 
     # get generate date and time
     now = datetime.now()
@@ -68,7 +72,7 @@ def build_file(page_layout: str):
         data = json.load(f)
 
     # build files
-    with open(HEADER_BASE, 'r') as header_in, open(header_output, 'w') as header_out:
+    with open(header_base, 'r') as header_in, open(header_output, 'w') as header_out:
         header_out.write(generate_statement)
         file_start = False
         for line in header_in:
@@ -78,7 +82,7 @@ def build_file(page_layout: str):
                 rendered = render_line(page_layout, data, line, data)
                 header_out.write(rendered)
 
-    with open(FOOTER_BASE, 'r') as footer_in, open(footer_output, 'w') as footer_out:
+    with open(footer_base, 'r') as footer_in, open(footer_output, 'w') as footer_out:
         footer_out.write(generate_statement)
         file_start = False
         for line in footer_in:
@@ -89,6 +93,10 @@ def build_file(page_layout: str):
                 footer_out.write(rendered)
 
 def main():
+
+    header_base = ""
+    footer_base = ""
+
     # remove build tree
     build_root = 'build'
     if os.path.exists(build_root):
@@ -96,13 +104,22 @@ def main():
     os.makedirs(build_root, exist_ok=True)
 
     build_options = {
-        "Site Specific": "site_specific",
-        "Site Specific: Cover Image": "site_specific_cover_image",
-        "Site Specific: Full Width": "site_specific_full_width",
-        "Site Specific: Landing Page": "site_specific_landing_page",
-        "Site Specific: Mobile Optimized": "site_specific_mobile_optimized",
+        # Site Specific build options
+        "Site Specific": "site_specific/site_specific",
+        "Site Specific: Cover Image": "site_specific/site_specific_cover_image",
+        "Site Specific: Full Width": "site_specific/site_specific_full_width",
+        "Site Specific: Landing Page": "site_specific/site_specific_landing_page",
+        "Site Specific: Mobile Optimized": "site_specific/site_specific_mobile_optimized",
         "Site Specific: Sidebar Breakout Landing":
-        "site_specific_sidebar_breakout_landing"
+        "site_specific/site_specific_sidebar_breakout_landing",
+
+        # Site Main build options
+        "Site Main: Cover Image": "site_main/site_main_cover_image",
+        "Site Main: Full Width": "site_main/site_main_full_width",
+        "Site Main: Inner": "site_main/site_main_inner",
+        "Site Main: Landing Page": "site_main/site_main_landing_page",
+        "Site Main: Mobile Optimized": "site_main/site_main_mobile_optimized",
+        "Site Main: Sidebar Breakout Landing": "site_main/site_main_sidebar_breakout_landing"
     }
 
     print("Which file to build?")
@@ -117,18 +134,32 @@ def main():
 
     if (selection == ALL):
         for option in build_options:
+            if "Site Specific" in option:
+                header_base = SITE_SPECIFIC_HEADER_BASE
+                footer_base = SITE_SPECIFIC_FOOTER_BASE
+            elif "Site Main" in option:
+                header_base = SITE_MAIN_HEADER_BASE
+                footer_base = SITE_MAIN_FOOTER_BASE
+
             selected_value = build_options[option]
 
             print("Building", selected_value)
-            build_file(selected_value)
+            build_file(selected_value, header_base, footer_base)
 
         print("Finished generating all files.")
     else:
         selected_key = options_list[int(selection)]
         selected_value = build_options[selected_key]
 
+        if "Site Specific" in selected_key:
+            header_base = SITE_SPECIFIC_HEADER_BASE
+            footer_base = SITE_SPECIFIC_FOOTER_BASE
+        elif "Site Main" in selected_key:
+            header_base = SITE_MAIN_HEADER_BASE
+            footer_base = SITE_MAIN_FOOTER_BASE
+
         print("Building ", selected_value)
-        build_file(selected_value)
+        build_file(selected_value, header_base, footer_base)
 
         print("Finished generating ", selected_value, " files.")
 
